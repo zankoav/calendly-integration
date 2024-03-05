76ed4a94b7a0fc06151b20e3421493c004f25546
Aleksandr.Zanko <aleksandr.zanko@fleetcor.com>
2024-03-05 11:58:20 +0300

    added calendly

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("zankoav", true);
-  Calendly.initBadgeWidget({
-    url: "https://calendly.com/zankoav/30min",
-    text: "Schedule time with me",
-    color: "#0069ff",
-    textColor: "#ffffff",
-    branding: true,
+  document.querySelectorAll("a.calendly-integration").forEach((element) => {
+    element.addEventListener(
+      "click",
+      function (event) {
+        event.preventDefault();
+        Calendly.initPopupWidget({ url: "https://calendly.com/zankoav/30min" });
+      },
+      false
+    );
+  });
+
+  function isCalendlyEvent(e) {
+    return (
+      e.origin === "https://calendly.com" &&
+      e.data.event &&
+      e.data.event.indexOf("calendly.") === 0
+    );
+  }
+
+  window.addEventListener("message", function (e) {
+    if (isCalendlyEvent(e)) {
+      /* Example to get the name of the event */
+      console.log("Event name:", e.data.event);
+
+      /* Example to get the payload of the event */
+      console.log("Event details:", e.data.payload);
+    }
   });
 });
