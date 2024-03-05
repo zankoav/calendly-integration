af64cf3a8e47869450cee7ec72258d0f5682c3e0
Aleksandr.Zanko <aleksandr.zanko@fleetcor.com>
2024-03-05 13:18:11 +0300

    Final

--- js/main.js
+++ js/main.js
@@ -1,39 +1,37 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelectorAll(".calendly-integration").forEach((element) => {
-    element.addEventListener(
-      "click",
-      function (event) {
-        event.preventDefault();
-        Calendly.initPopupWidget({
-          url: "https://calendly.com/eugene-vab/30min",
-        });
-      },
-      false
-    );
-  });
+document.querySelectorAll(".calendly-integration").forEach((element) => {
+  element.addEventListener(
+    "click",
+    function (event) {
+      event.preventDefault();
+      Calendly.initPopupWidget({
+        url: "https://calendly.com/eugene-vab/30min",
+      });
+    },
+    false
+  );
+});
 
-  function isCalendlyEvent(e) {
-    return (
-      e.origin === "https://calendly.com" &&
-      e.data.event &&
-      e.data.event.indexOf("calendly.") === 0
-    );
-  }
+function isCalendlyEvent(e) {
+  return (
+    e.origin === "https://calendly.com" &&
+    e.data.event &&
+    e.data.event.indexOf("calendly.") === 0
+  );
+}
 
-  window.addEventListener("message", function (e) {
-    if (isCalendlyEvent(e)) {
-      /* Example to get the name of the event */
-      //   console.log("Event name:", e.data.event);
+window.addEventListener("message", function (e) {
+  if (isCalendlyEvent(e)) {
+    /* Example to get the name of the event */
+    //   console.log("Event name:", e.data.event);
 
-      if (e.data.event == "calendly.event_scheduled") {
-        window.dataLayer = window.dataLayer || [];
-        window.dataLayer.push({
-          event: "calendly.event_scheduled",
-        });
-      }
-
-      /* Example to get the payload of the event */
-      //   console.log("Event details:", e.data.payload);
+    if (e.data.event == "calendly.event_scheduled") {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "Calendly_Scheduled",
+      });
     }
-  });
+
+    /* Example to get the payload of the event */
+    //   console.log("Event details:", e.data.payload);
+  }
 });
